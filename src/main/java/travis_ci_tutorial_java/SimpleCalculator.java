a9193cf63c9a286d5677a78fcbe44220a53fad2e
ChiOnugu <terrence.chinedu@gmail.com>
2020-02-22 01:43:00 -0500

    Updates

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -1,7 +1,18 @@
 package travis_ci_tutorial_java;
 
-public class SimpleCalculator {
-	public int add(int a, int b) {
+public class SimpleCalculator
+
+{
+	public int add(int a, int b)
+
+	{
 		return a + b;
 	}
+
+	public int minus(int a, int b)
+
+	{
+		return a - b;
+	}
+
 }
