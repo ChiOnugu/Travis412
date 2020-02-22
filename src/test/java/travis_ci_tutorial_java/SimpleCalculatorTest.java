5af8fbc1b4d4ba74bc603c9318c96ee277641cac
ChiOnugu <terrence.chinedu@gmail.com>
2020-02-22 01:29:26 -0500

    Updates

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -5,10 +5,25 @@ import java.util.*;
 import static org.junit.Assert.*;
 import org.junit.*;
 
-public class SimpleCalculatorTest {
+public class SimpleCalculatorTest
+
+{
+
 	@Test
-	public void testAdd() {
+	public void testAdd()
+
+	{
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
 	}
+
+	@Test
+	public void testMinus()
+
+	{
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.minus(5, 3), 2);
+	}
+
 }
+
